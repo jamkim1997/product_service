86feb22a4a97bacbda9c87e38b34b4af58da775f
jamkim1997 <jamkim1997@gmail.com>
2023-08-15 12:08:28 +1000

    Microservice main project created

---
+++ inventory-service/src/main/java/com/example/inventoryservice/model/Inventory.java
@@ -0,0 +1,24 @@
+package com.example.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
